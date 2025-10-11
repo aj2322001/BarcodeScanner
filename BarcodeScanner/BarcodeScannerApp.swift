3563c12779df4ccf5535cb36e48b20c33de1f722
Archit Joshi <archit.joshi@anarock.com>
2025-10-11 17:50:50 +0530

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Archit  Joshi on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
